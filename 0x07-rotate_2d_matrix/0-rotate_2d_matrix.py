d3e228e607cdfcbc0b7bbefe321d15239c102784
ZainAalabidin <zynly9303@gmail.com>
2024-09-28 18:22:38 +0000

    ok

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -8,11 +8,3 @@ def rotate_2d_matrix(matrix):
     """
     for x, y in enumerate(zip(*reversed(matrix))):
         matrix[x] = list(y)
-
-
-if __name__ == '__main__':
-    matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-
-    """ rotate_2d_matrix(matrix) """
-    rotate_2d_matrix(matrix)
-    print(matrix)
